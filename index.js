d69c33dcc9747d0dfe1e15954350484f271d970d
meetsavani07 <146066645+meetsavani07@users.noreply.github.com>
2024-01-31 22:50:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let signup = document.querySelector(".Signup");
+let login = document.querySelector(".Signin");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+ 
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+ 
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
